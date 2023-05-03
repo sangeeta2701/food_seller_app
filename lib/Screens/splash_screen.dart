8dffa8b1611c7149ed0d8c3891406f2048c2a753
DELL <anjugupta7060@gmail.com>
2023-05-03 21:59:40 +0530

    Seller App

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:food_seller_app/authentication/auth_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTimer() {
+    Timer(Duration(seconds: 4), () async {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => AuthScreen(),
+        ),
+      );
+    });
+  }
+
+  @override
+  void initState() {
+    startTimer();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/splash.jpg"),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Text(
+                  "Sell Food Online",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(
+                    fontSize: 40,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/authentication/auth_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Welcome"),
+      ),
+    );
+  }
+}
