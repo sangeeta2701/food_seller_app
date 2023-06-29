8edb025736f038fb13426ebf4a35faad3c6bfcfa
DELL <anjugupta7060@gmail.com>
2023-06-29 19:43:40 +0530

    Seller App

--- lib/authentication/login_screen.dart
+++ lib/authentication/login_screen.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:food_seller_app/global/global.dart';
+import 'package:food_seller_app/mainScreens/home_screen.dart';
+import 'package:food_seller_app/widgets/error_dialog.dart';
+import 'package:food_seller_app/widgets/loading_dialog.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../utils/colors.dart';
 import '../widgets/customTextField.dart';
-
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -16,6 +21,66 @@ class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  formValidation() {
+    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      //login
+      loginNow();
+    } else {
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(message: "Please write email/password");
+          });
+    }
+  }
+
+  loginNow() async {
+    showDialog(
+        context: context,
+        builder: (c) {
+          return LoadingDialog(message: "Cheaking Credentials");
+        });
+    User? currentUser;
+    await firebaeAuth
+        .signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    )
+        .then((auth) {
+      currentUser = auth.user!;
+    }).catchError((error) {
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c) {
+            return ErrorDialog(message: error.message.toString());
+          });
+    });
+    if (currentUser != null) {
+      readDataAndSetDataLocally(currentUser!).then((value) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      });
+    }
+  }
+
+  Future readDataAndSetDataLocally(User currentUser) async {
+    await FirebaseFirestore.instance
+        .collection("sellers")
+        .doc(currentUser.uid)
+        .get()
+        .then((snapshot) async {
+      await sharedPreferences!.setString("uid", currentUser.uid);
+      await sharedPreferences!
+          .setString("email", snapshot.data()!["sellerEmail"]);
+      await sharedPreferences!
+          .setString("name", snapshot.data()!["sellerName"]);
+      await sharedPreferences!
+          .setString("photoUrl", snapshot.data()!["SellerAvatarUrl"]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -24,8 +89,12 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           Container(
             alignment: Alignment.center,
-            child: Padding(padding: EdgeInsets.all(15),
-            child: Image.asset("assets/images/seller.png",height: 250,),
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Image.asset(
+                "assets/images/seller.png",
+                height: 250,
+              ),
             ),
           ),
           Form(
@@ -33,44 +102,41 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Column(
               children: [
                 CustomTextField(
-                        controller: emailController,
-                        data: Icons.email,
-                        hintText: "Email",
-                        enabled: true,
-                        isObsecure: false),
+                    controller: emailController,
+                    data: Icons.email,
+                    hintText: "Email",
+                    enabled: true,
+                    isObsecure: false),
                 CustomTextField(
-                        controller: passwordController,
-                        data: Icons.lock,
-                        hintText: "Password",
-                        enabled: true,
-                        isObsecure: true),
+                    controller: passwordController,
+                    data: Icons.lock,
+                    hintText: "Password",
+                    enabled: true,
+                    isObsecure: true),
               ],
-            ),),
-            Padding(
+            ),
+          ),
+          Padding(
             padding: const EdgeInsets.symmetric(vertical: 12),
             child: ElevatedButton(
-              
               onPressed: () {
-                
-                print("Clicked");
+                formValidation();
               },
               style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 80,vertical: 10),
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
                 primary: wColor,
               ),
               child: Text(
                 "Log In",
                 style: GoogleFonts.poppins(
-                  color: orange500Color,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16
-                ),
+                    color: orange500Color,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
               ),
             ),
           )
         ],
       ),
-      
     );
   }
 }
